96d6d5f7fb0d54937394f84135152e1cbaf2ccee
CaioIbraim <caiofabiocosta@gmail.com>
2017-10-20 16:36:50 -0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,63 @@
+// preparing language file
+var aLangKeys=new Array();
+aLangKeys['en']=new Array();
+aLangKeys['fr']=new Array();
+aLangKeys['pt-br']=new Array();
+
+
+
+aLangKeys['en']['home']='Home';
+aLangKeys['en']['peoples']='Peoples >>';
+aLangKeys['en']['all_list']='All list';
+aLangKeys['en']['online']='Online';
+aLangKeys['en']['articles']='Articles >>';
+aLangKeys['en']['js']='JavaScript';
+aLangKeys['en']['php']='PHP';
+aLangKeys['en']['html']='HTML';
+aLangKeys['en']['css']='CSS';
+aLangKeys['en']['contact_us']='Contact us';
+aLangKeys['en']['welcome']='Welcome guests';
+aLangKeys['en']['a_man']='A man bribes a rabbit with wicked dentures to run away with him in a sailboat via an ambulance. Bribing Koalas to remain illegally in one place. Trees anchor me in place. / Your mom drives the ambulance, but the city is farther than it appears.';
+
+
+aLangKeys['pt-br']['home']='Inicio';
+aLangKeys['pt-br']['peoples']='Pessoas >>';
+aLangKeys['pt-br']['all_list']='Lista completa';
+aLangKeys['pt-br']['online']='Online';
+aLangKeys['pt-br']['articles']='Artios >>';
+aLangKeys['pt-br']['js']='JavaScript';
+aLangKeys['pt-br']['php']='PHP';
+aLangKeys['pt-br']['html']='HTML';
+aLangKeys['pt-br']['css']='CSS';
+aLangKeys['pt-br']['contact_us']='Entre em contato';
+aLangKeys['pt-br']['welcome']='Bem-vindo visitante';
+aLangKeys['pt-br']['a_man']='Um homem suborna um coelho com próteses perversas para fugir com ele em um veleiro através de uma ambulância. Subornando Koalas para permanecerem ilegalmente em um só lugar. As árvores me ancoram no lugar. / Sua mãe dirige a ambulância, mas a cidade está mais longe do que parece.';
+
+
+
+aLangKeys['fr']['home']='Accueil';
+aLangKeys['fr']['peoples']='Peuples >>';
+aLangKeys['fr']['all_list']='Toutes les listes';
+aLangKeys['fr']['online']='En ligne';
+aLangKeys['fr']['articles']='Articles >>';
+aLangKeys['fr']['js']='JavaScript';
+aLangKeys['fr']['php']='Php';
+aLangKeys['fr']['html']='Html';
+aLangKeys['fr']['css']='Css';
+aLangKeys['fr']['contact_us']='Contactez nous';
+aLangKeys['fr']['welcome']='Bienvenue aux invites';
+aLangKeys['fr']['a_man']="Un homme soudoie un lapin avec des prothèses méchantes pour s'enfuir avec lui dans un voilier via une ambulance. Corruption des Koalas pour qu'ils restent illégalement à un endroit. Les arbres m'ancrent en place. / Votre mère conduit l'ambulance, mais la ville est plus loin qu'il n'y paraît.";
+
+
+
+
+$(document).ready(function() {
+// onclick behavior
+$('.lang').click( function() {
+var lang = $(this).attr('id'); // obtain language id
+// translate all translatable elements
+$('.tr').each(function(i){
+$(this).text(aLangKeys[lang][ $(this).attr('key') ]);
+});
+} );
+});
